de158335ee7a42ea33f786443dd8741d0db75d2e
Ashok Emani <ashok.emani@intel.com>
2018-01-10 23:23:19 -0800

    init

---
+++ src/main/main.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int> vi;
+    vi.push_back(5);
+    cout << vi[0];
+
+    cout << "\nEnd\n";
+}
---
+++ src/test/test.cpp
@@ -0,0 +1,10 @@
+#include <main.h>
+
+#include <gtest/gtest.h>
+
+// this is incomplete
+TEST(MyTestSuit, MyTestCase) {
+  EXPECT_EQ(1, 1);
+}
+
+
